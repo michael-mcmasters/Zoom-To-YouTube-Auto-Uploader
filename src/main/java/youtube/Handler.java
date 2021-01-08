e089166931e9ab668674b2c1142f639bdd672082
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-08 15:30:24 -0500

    Reorganize Handler class

--- src/main/java/youtube/Handler.java
+++ src/main/java/youtube/Handler.java
@@ -32,31 +32,28 @@ public class Handler {
             if (file != null) {
                 System.out.println("Found new file, + " + file.getName());
                 System.out.println("Pausing program for x minutes before uploading to give mp4 file time to decode.");
-
-                directoryScanner.moveFileToFolder(file, Folder.VIDEOS_UNABLE_TO_UPLOAD);
-
-                // Set one hour delay for zoom video to decode.
-                delay(3600);
+                delay(3600);     // Set one hour delay for zoom video to decode.
 
                 System.out.println("Program resume. Preparing to upload to Youtube ...");
 
-
-                // try uploading to youtube. return true or false if it worked.
-                // If it worked, add to videos-uploaded directory.
-                // If it didn't work, add to videos-unable-to-upload directory.
-                //System.out.println("Success! Video uploaded to Youtube.");
-                //System.out.println("Upload to Youtube failed");
+                boolean videoUploaded = false;
+                if (videoUploaded) {
+                    System.out.println("Success! Video uploaded to Youtube.");
+                    directoryScanner.moveFileToFolder(file, Folder.VIDEOS_UPLOADED);
+                } else {
+                    System.out.println("Upload to Youtube failed");
+                    directoryScanner.moveFileToFolder(file, Folder.VIDEOS_UNABLE_TO_UPLOAD);
+                }
             } else {
                 System.out.println("No new file found.");
                 System.out.println("End loop. Will loop again in x minutes.");
             }
 
-            // Delay so loop isn't running constantly.
-            delay(1800);
+            delay(1800);     // Delay loop to only run every 30 minutes.
         }
     }
 
-    // 60s is one minute, 120 is two minutes, 3600 is an hour, etc.
+    // 60ms is one minute, 120ms is two minutes, 3600ms is an hour, etc.
     private void delay(int milliseconds) {
         try {
             Thread.sleep(milliseconds * 1000);
